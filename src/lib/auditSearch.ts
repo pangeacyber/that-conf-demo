da4a1fb7954e3ad84f504c10825e5bc88439c2f2
vanpangea <vanessa.villa@pangea.cloud>
2024-07-22 13:29:02 -0700

    Get emails

--- src/lib/auditSearch.ts
+++ src/lib/auditSearch.ts
@@ -1,29 +1,71 @@
 /* eslint-disable no-console */
 
 import { PangeaConfig, AuditService, PangeaErrors} from 'pangea-node-sdk';
-import { parseZonedDateTime, ZonedDateTime } from "@internationalized/date";
+import { ZonedDateTime } from "@internationalized/date";
+import { Console } from 'console';
 
 export default async function auditSearch({
   startTime,
   endTime }: any) {
 
- let s = new ZonedDateTime(startTime);
+ let start = new ZonedDateTime(
+  startTime.year, 
+  startTime.month, 
+  startTime.day,
+  startTime.timeZone,
+  startTime.offset,
+  startTime.hour,
+  startTime.minute,
+  startTime.second);
 
-  console.log("Stringified Start " + JSON.stringify(data.start))
-  console.log("End time: " + (data.end).toString())
+  let end = new ZonedDateTime(
+    endTime.year, 
+    endTime.month, 
+    endTime.day,
+    endTime.timeZone,
+    endTime.offset,
+    endTime.hour,
+    endTime.minute,
+    endTime.second);
 
-  
+  console.log("start time: " + (start).toAbsoluteString())
+  console.log("end time: " + (end).toAbsoluteString())
 
-  if(process.env.PANGEA_TOKEN) {
-    const token = process.env.PANGEA_TOKEN as string;
+  if(process.env.PANGEA_SAL_TOKEN) {
+    const token = process.env.PANGEA_SAL_TOKEN as string;
     const config_id = process.env.PANGEA_AUDIT_CONFIG as string;
 
     const config = new PangeaConfig({ domain: process.env.PANGEA_DOMAIN, configID: config_id });
     const audit = new AuditService(token, config);
 
     try {
-      //const logResponse = await audit.search('',{end: endTime, start: startTime}, {});
-      //console.log("Response: %s", logResponse.result);
+      const logResponse = await audit.search('',{end: (end).toAbsoluteString() , start: (start).toAbsoluteString()}, {});
+      //console.log("Response: %s", JSON.stringify(logResponse.result));
+
+
+      if(logResponse.success) {
+        console.log("SUCCESS");
+        logResponse.result.events.forEach(element => {
+
+          //console.log(element.envelope.event.external_context)
+
+          let service_feat = element.envelope.event.action.toString()
+          let context_str = element.envelope.event.external_context.toString()
+          let context = JSON.parse(element.envelope.event.external_context.toString())
+
+          if (service_feat.includes("login")) {
+            console.log("PERSON" + context.actor.username)
+          
+            if(context_str.includes("intelligence"))
+              console.log("found")
+              //console.log("Place "+ JSON.stringify(context.request.intelligence.))
+          }
+          
+        });
+          
+        //let locations = getLocations(logResponse.result.events);
+
+      }
     } catch (err) {
       if (err instanceof PangeaErrors.APIError) {
         console.log(err.summary, err.pangeaResponse);
@@ -33,5 +75,21 @@ export default async function auditSearch({
     }
   } else {
     return false;
+  }
+}
+
+
+function getLocations(res: any){
+
+  console.log("IN Get Locations")
+
+  let result = res;
+
+  console.log(result)
+
+  for(let key in result){
+
+    if(key.includes("external_context"))
+      console.log( key + ": " + result[key])
   }
 }
--- src/pages/api/audit-search.ts
+++ src/pages/api/audit-search.ts
@@ -3,7 +3,6 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.body) {
-        console.log("IN the /API" + JSON.stringify(req.body))
         const auditResp = await auditSearch(req.body)
         if (auditResp != false) {
             res.status(200).json({ message: "sucess" })
