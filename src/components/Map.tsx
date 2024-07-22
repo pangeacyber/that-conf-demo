b5dcbc55b3aa2e96b5bc7559f49357abc421c2f6
vanpangea <vanessa.villa@pangea.cloud>
2024-07-22 15:58:26 -0700

    Get the appropriate Info and have put into an array.

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -5,12 +5,13 @@ import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 import "leaflet-defaulticon-compatibility";
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React from "react";
 
 export default function Map() {
-    
+
     return(
         <MapContainer 
-            center={[51.505, -0.09]} 
+            center={[42.515, -96.79]} 
             zoom={11} 
             scrollWheelZoom={true}
             style={{ height: "400px", width: "600px" }}
@@ -19,12 +20,12 @@ export default function Map() {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[51.505, -0.09]}>
+         
+            <Marker position={[42.515, -96.79]}>
                 <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
+                  email @ <br /> Time? Easily customizable.
                 </Popup>
             </Marker>
         </MapContainer>
     )
-
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export interface login_marker {
+ username: string;
+ lat: number;
+ long: number;
+ time: string;
+}
